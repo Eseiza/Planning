c5d36a0afe79e55c760b16ae50c68e3af59a66aa
Nay <168801233+Eseiza@users.noreply.github.com>
2026-05-28 16:59:30 -0300

    Update planner.js

--- planner.js
+++ planner.js
@@ -1,28 +1,13 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
-
     <meta charset="UTF-8">
-
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
-
-    <title>
-        Planner Obras Romero
-    </title>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Planner Obras Romero </title>
     <link rel="stylesheet" href="style.css">
-
     <!-- FULLCALENDAR -->
-
-    <link
-        href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.css"
-        rel="stylesheet"
-    >
-
+    <link href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.css"rel="stylesheet">
 </head>
-
 <body>
 
     <!-- NAVBAR -->
